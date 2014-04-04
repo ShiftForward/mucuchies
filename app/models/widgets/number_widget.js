5d196a6347ec58e0965e364a154d52f0a85b7277
Joao Azevedo <joao.c.azevedo@gmail.com>
2014-04-05 00:01:18 +0100

    Use the SmoothHelper in the NumberWidget.

--- app/models/widgets/number_widget.js
+++ app/models/widgets/number_widget.js
@@ -4,32 +4,33 @@
  *
  * Expects data from sources as a single number.
  */
-Dashboard.NumberWidget = Dashboard.Widget.extend({
+Dashboard.NumberWidget = Dashboard.Widget.extend(Dashboard.SmoothHelper, {
   sourceData: 0,
   previous: 0,
   templateName: 'number_widget',
   classNames: ['widget', 'widget-number'],
   preUnit: "",
   postUnit: "",
+  currentValue: 0,
+  differencePercentage: 0,
+  arrow: 'icon-arrow-down',
+
+  onChangeContent: function() {
+    this.setSmooth('currentValue', this.get('content'));
+    if (this.get('previous') != 0)
+      this.setSmooth('differencePercentage', Math.abs(Math.round((this.get('content') - this.get('previous')) / this.get('previous') * 100)));
+    if (this.get('content') > this.get('previous'))
+      this.set('arrow', 'icon-arrow-up');
+    else
+      this.set('arrow', 'icon-arrow-down');
+    this.set('previous', this.get('content'));
+  }.observes('content'),
 
   formattedValue: function() {
-    return this.get("preUnit") + this.get("content") + this.get("postUnit");
-  }.property("content"),
+    return this.get('preUnit') + Math.round(this.get('currentValue')) + this.get('postUnit');
+  }.property('currentValue'),
 
   difference: function() {
-    var res = "";
-    if (this.get("previous") != 0) {
-      res = Math.abs(Math.round((this.get("content") - this.get("previous")) / this.get("previous") * 100));
-      res += "%";
-    }
-    this.set("previous", this.get("content"));
-    return res;
-  }.property("content"),
-
-  arrow: function() {
-    if (this.get("content") > this.get("previous"))
-      return "icon-arrow-up";
-    else
-      return "icon-arrow-down";
-  }.property("content")
+    return Math.round(this.get('differencePercentage')) + "%";
+  }.property('differencePercentage')
 });
