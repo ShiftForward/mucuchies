c8cbc48b9a8376c8193cacdc3de59bf7dc2e08b3
Bjesus1 <jesus.silva.bruno@hotmail.com>
2014-06-05 16:32:13 +0100

    Update stock_widget.js

--- app/models/widgets/stock_widget.js
+++ app/models/widgets/stock_widget.js
@@ -5,7 +5,7 @@
 Dashboard.StockWidget = Dashboard.Widget.extend({
   sourceData: [],
     
-  Stockerfiller: function(){
+  stockerFiller: function(){
     var c = this.get("content");
 
     for(var i=0; i < c.lenght; i++){
