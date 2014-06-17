d3a23c4fef7944df0426a322977c4dd04064b120
Bruno Silva <jesus.silva.bruno@hotmail.com>
2014-06-17 17:15:22 +0100

    New ticket widget using stock source

---
+++ app/models/widgets/ticker_widget.js
@@ -0,0 +1,12 @@
+/*
+//
+//
+*/
+Dashboard.TickerWidget = Dashboard.Widget.extend({
+  sourceData: [],
+
+  showLastUpdated: true,
+  templateName: 'ticker_widget',
+  classNames: ['widget', 'widget-ticker']
+  
+});
